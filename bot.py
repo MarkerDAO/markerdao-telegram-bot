2e176dd2caab502948c04b0b9c127ee03e95fe2f
MathxH Chen <brainfvck@foxmail.com>
2023-03-06 10:38:36 +0800

    support chat.type supergroup

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ async def message_handle(update: Update, context: CallbackContext, message=None)
     print("from telegram bot update message: ", message)
     print ("chat type: ", message.chat.type)
     message_text = message.text
-    if message.chat.type == "group" and config.telegram_bot_name in message_text:
+    if (message.chat.type == "group" or message.chat.type == "supergroup") and config.telegram_bot_name in message_text:
         message_text = message_text.replace(config.telegram_bot_name, "")
         answer = chat_gpt.get_answer_from_chatgpt(message_text)
         print("answer: ", answer.strip())
